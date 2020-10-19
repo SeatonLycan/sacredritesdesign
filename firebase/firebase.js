fc7ac029d57c5ca133f66783310872d93f66dc1c
Seaton <slycan42@gmail.com>
2020-10-18 19:52:16 -0600

    Initialize firebase

---
+++ firebase/firebase.js
@@ -0,0 +1,23 @@
+// Firebase App (the core Firebase SDK) is always required and
+// must be listed before other Firebase SDKs
+var firebase = require("firebase/app");
+
+// Add the Firebase products that you want to use
+require("firebase/auth");
+require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCKCpgLdlUso9M-C-cdfKmGdkSGRh41yzY",
+  authDomain: "sacred-rites-jewelry.firebaseapp.com",
+  databaseURL: "https://sacred-rites-jewelry.firebaseio.com",
+  projectId: "sacred-rites-jewelry",
+  storageBucket: "sacred-rites-jewelry.appspot.com",
+  messagingSenderId: "426780987510",
+  appId: "1:426780987510:web:a1eab22329228753450f57"
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+export const db = firebase.firestore();

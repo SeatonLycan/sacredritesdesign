511302c9b1a65fe10ab75a97cb8f7b39a1ca9c7d
Seaton <slycan42@gmail.com>
2020-10-19 18:26:17 -0600

    Update Navbar to add menu list on smaller screens

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,9 +1,22 @@
+import { useState } from 'react'
 import Link from 'next/link'
 import Divider from '@material-ui/core/Divider'
 import useStyles from './Navbar.style'
+import IconButton from '@material-ui/core/IconButton'
+import MenuIcon from '@material-ui/icons/Menu'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
 
 export default function Navbar() {
   const classes=useStyles()
+  const [menuAnchorElement, setMenuAnchorElement] = useState(null)
+
+  const handleSmallMenuClick = (event) => {
+    setMenuAnchorElement(event.currentTarget)
+  }
+  const handleCloseSmallMenu = () => {
+    setMenuAnchorElement(null)
+  }
 
   return (
       <>
@@ -21,7 +34,7 @@ export default function Navbar() {
             <Link href='/about'>
                 <a className={classes.a}>About</a>
             </Link>
-            <Link href='/'>
+            <Link href='/contact'>
                 <a className={classes.a}>Contact</a>
             </Link>
             <Divider />
@@ -32,6 +45,53 @@ export default function Navbar() {
       </div>
       <div className={classes.container}>
         <h1 className={classes.header}>Sacred Rites Jewelry</h1>
+      </div>
+
+      <div className={classes.containerSmall}>
+        <div className={classes.headerLeftSmall}>
+          <IconButton className={classes.iconButton} edge="start" color="primary" aria-label="menu" 
+              onClick={(event)=>{handleSmallMenuClick(event)}}>
+            <MenuIcon className={classes.menuIcon}/>
+          </IconButton>
+          <Menu
+              id="simple-menu"
+              anchorEl={menuAnchorElement || null}
+              keepMounted
+              open={Boolean(menuAnchorElement)}
+              onClose={handleCloseSmallMenu}
+            >
+              <MenuItem onClick={() => {handleCloseSmallMenu()}}>
+                <Link href='/'>
+                  <a>Shop</a>
+                </Link>
+              </MenuItem>
+              <Divider />
+              <MenuItem onClick={() => {handleCloseSmallMenu()}}>
+                <Link href='/'>
+                  <a>Features</a>
+                </Link>
+              </MenuItem>
+              <Divider />
+              <MenuItem onClick={() => {handleCloseSmallMenu()}}>
+                <Link href='/custom-orders'>
+                  <a>Custom Orders</a>
+                </Link>
+              </MenuItem>
+              <Divider />
+              <MenuItem onClick={() => {handleCloseSmallMenu()}}>
+                <Link href='/about'>
+                  <a>About</a>
+                </Link>
+              </MenuItem>
+              <Divider />
+              <MenuItem onClick={() => {handleCloseSmallMenu()}}>
+                <Link href='/contact'>
+                  <a>Contact</a>
+                </Link>
+              </MenuItem>
+            </Menu>
+        </div>
+        <div className={classes.headerCenterSmall}>Sacred Rites Jewelry</div>
       </div>
     </>
   );
--- components/Navbar.style.js
+++ components/Navbar.style.js
@@ -7,9 +7,50 @@ export default makeStyles((theme) => createStyles({
       justifyContent: 'center',
       position: 'static',
       zIndex: 10,
+      [theme.breakpoints.down('sm')]: {
+        display: 'none'
+      },
+    },
+    containerSmall : {
+      display: 'flex',
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      position: 'static',
+      zIndex: 10,
+      backgroundColor: 'black',
+      [theme.breakpoints.up('md')]: {
+        display: 'none'
+      },
     },
     main: {
       marginTop: '15px',
+    },
+    menuIcon: {
+      color: 'white',
+      marginLeft: '15px'
+    },
+    headerLeftSmall: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      [theme.breakpoints.up('md')]: {
+        display: 'none',
+      }
+    },
+    headerCenterSmall: {
+      flex: 1,
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+      color: 'white',
+      fontSize: '24px',
+      [theme.breakpoints.down('sm')]: {
+        marginLeft: -52,
+      },
+      [theme.breakpoints.down('xs')]: {
+        marginLeft: -42,
+        fontSize: '16px'
+      },
     },
     logo: {
       height: "80px",
@@ -17,24 +58,22 @@ export default makeStyles((theme) => createStyles({
       marginTop: "15px",
     },
     header: {
-    fontFamily: "Sorts Mill Goudy",
-    fontSize: "17px",
-    fontWeight: 400,
-    fontStyle: 'normal',
-    textTransform: 'none',
-    letterSpacing: ".31px",
-    lineHeight: "1.6em"
+      fontFamily: "Sorts Mill Goudy",
+      fontSize: "17px",
+      fontWeight: 400,
+      fontStyle: 'normal',
+      textTransform: 'none',
+      letterSpacing: ".31px",
+      lineHeight: "1.6em"
     },
     a : {
-    color: 'black',
-    textDecoration: 'none',
-    margin: '10px',
-    fontFamily: 'proximaDova',
-    fontFize: '12px',
-    fontHeight: '400',
-    fontStyle: 'normal',
-    textTransform: 'uppercase',
-    letterSpacing: '1px',
-    lineHeight: '1.3em',
+      margin: '20px',
+      fontFamily: 'proximaDova',
+      fontFize: '12px',
+      fontHeight: '400',
+      fontStyle: 'normal',
+      textTransform: 'uppercase',
+      letterSpacing: '1px',
+      lineHeight: '1.3em',
     }
   }));

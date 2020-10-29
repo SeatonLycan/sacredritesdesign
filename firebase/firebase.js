f15c70c218b4b20faf68088ef15b27330aed6a26
Seaton <slycan42@gmail.com>
2020-10-29 16:24:06 -0600

    Update firebase config to include auth

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -1,10 +1,10 @@
 // Firebase App (the core Firebase SDK) is always required and
 // must be listed before other Firebase SDKs
-var firebase = require("firebase/app");
+var firebase = require("firebase/app")
 
 // Add the Firebase products that you want to use
-require("firebase/auth");
-require("firebase/firestore");
+require("firebase/auth")
+require("firebase/firestore")
 
 const firebaseConfig = {
   apiKey: "AIzaSyCKCpgLdlUso9M-C-cdfKmGdkSGRh41yzY",
@@ -14,10 +14,11 @@ const firebaseConfig = {
   storageBucket: "sacred-rites-jewelry.appspot.com",
   messagingSenderId: "426780987510",
   appId: "1:426780987510:web:a1eab22329228753450f57"
-};
-
-if (!firebase.apps.length) {
-  firebase.initializeApp(firebaseConfig);
 }
 
-export const db = firebase.firestore();
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export const db = firebase.firestore()
+export const auth = firebase.auth()

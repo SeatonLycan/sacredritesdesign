3e97f6743d5f49807f0a0f7d228d8bb92d819e15
Seaton <slycan42@gmail.com>
2020-11-19 11:27:42 -0700

    Create default SEO config

---
+++ SEO/next-seo.config.js
@@ -0,0 +1,22 @@
+export default {
+    openGraph: {
+      title: 'Sacred Rites Jewelry',
+      type: 'website',
+      locale: 'en_IE',
+      url: 'https://www.sacredritesjewelry.vercel.app',
+      site_name: 'Sacred Rites Jewelry',
+      description: 'Denver-based silversmith and lapidary. Rare gemstones for unique and magical silver jewelry.',
+      images: [
+        {
+          url: "https://firebasestorage.googleapis.com/v0/b/sacred-rites-jewelry.appspot.com/o/siteLogo%2FSR_SNAKE_2.jpg?alt=media&token=fd0a4537-7133-49ed-9e06-014016d3d64b",
+          width: 800,
+          height: 600,
+          alt: 'Sacred Rites Logo'
+        }
+      ]
+    },
+    twitter: {
+      site: 'SacredRitesJewelry.com',
+      cardType: 'summary_large_image',
+    },
+  } 

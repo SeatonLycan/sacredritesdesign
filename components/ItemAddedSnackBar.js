dc5ed10d6f1993a1d62c960b9946a51c0c4e561d
Seaton <slycan42@gmail.com>
2020-12-14 12:26:04 -0700

    Update to include link to shopping-cart page

--- components/ItemAddedSnackBar.js
+++ components/ItemAddedSnackBar.js
@@ -3,9 +3,9 @@ import Snackbar from '@material-ui/core/Snackbar'
 import IconButton from '@material-ui/core/IconButton'
 import CloseIcon from '@material-ui/icons/Close'
 import { SnackbarContent } from '@material-ui/core'
+import Link from 'next/link'
 
 export default function itemAddedSnackbar(props) {
-
     const action = (
         <IconButton size="small" aria-label="close" color="primary" onClick={props.handleCloseSnackbar}>
               <CloseIcon fontSize="small" style={{color: 'white'}}/>
@@ -13,20 +13,19 @@ export default function itemAddedSnackbar(props) {
     )
   return (
     <div>
-      <Snackbar
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right',
-        }}
-        open={props.openSnackBar}
-        autoHideDuration={3000}
-        onClose={props.handleCloseSnackbar}
-      >
-        <SnackbarContent style={{backgroundColor: 'black'}}
-        message="Item Added to Cart"
-        action={action}
-        />
-      </Snackbar>
+        <Snackbar
+          anchorOrigin={{
+            vertical: 'bottom',
+            horizontal: 'right',
+          }}
+          open={props.openSnackBar}
+          autoHideDuration={3000}
+          onClose={props.handleCloseSnackbar}
+        >
+          <SnackbarContent style={{backgroundColor: 'black'}}
+          message={<Link href='/shopping-cart'><a style={{color: 'white'}}>Item Added to Cart</a></Link>}
+          action={action} />
+        </Snackbar>
     </div>
   )
 }
